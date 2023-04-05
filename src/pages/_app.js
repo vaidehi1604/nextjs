d39e57496d2344cafeee3ce6592149f09b094201
vaidehi1604 <vaidehip@zignuts.com>
2023-04-05 11:27:18 +0530

    "_app.js":add footer and navbar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,42 +1,54 @@
-import '@/styles/globals.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Head from 'next/head'
+import "@/styles/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Head from "next/head";
 
 import { useEffect, useState } from "react";
 
-
-
-
 export default function App({ Component, pageProps }) {
   <Head>
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"/>
-  </Head>
+    <meta
+      name="viewport"
+      content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0"
+    />
+  </Head>;
   useEffect(() => {
-    console.log("i am use effect")
+    console.log("Home Page");
   }, []);
-  
-  const [cart,setCart] = useState([]);
+
+  const [cart, setCart] = useState([]);
   const [reloadKey, setReloadKey] = useState(1);
 
-  const addToCart=(item,qty,price)=>{
+  const addToCart = (item, qty, price) => {
     console.log("addd to cart");
-    let newCart=cart 
+    let newCart = cart;
     for (let index = 0; index < qty; index++) {
       // const element = qty;
-      newCart.push([item,price])
+      newCart.push([item, price]);
     }
-   console.log(newCart);
-    setCart(newCart)
-    setReloadKey(Math.random())
-  }
-  const removeFromCart=(item,qty)=>{
-    let newCart=cart 
-    let index=newCart.indexOf(item)
-    newCart.splice(index)
-    setCart(newCart)
-  }
-  const clearCart=(item)=>{}
+    console.log(newCart);
+    setCart(newCart);
+    setReloadKey(Math.random());
+  };
+  const removeFromCart = (item) => {
+    let newCart = cart;
+    let index = newCart.indexOf(item);
+    newCart.splice(index);
+    setCart(newCart);
+  };
+  const clearCart = (item) => {};
 
-  return  <><Navbar  key={reloadKey} cart={cart}/> <Component cart={cart} removeFromCart={removeFromCart}  addToCart={addToCart} clearCart={clearCart} {...pageProps} /><Footer/></>
+  return (
+    <>
+      <Navbar key={reloadKey} cart={cart} />
+      <Component
+        cart={cart}
+        removeFromCart={removeFromCart}
+        addToCart={addToCart}
+        clearCart={clearCart}
+        {...pageProps}
+      />
+      <Footer />
+    </>
+  );
 }
